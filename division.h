4e187ad088afd6eee8a271ce2f3a739d871e471a
ӨМИРСЕРИК БАЗАРХАН <bazarkhanumirsyerik@gmail.com>
2025-04-16 21:12:11 +0800

    Update division.h

--- division.h
+++ division.h
@@ -4,12 +4,18 @@
 #include <string>
 using namespace std;
 
+// Division класс нь компанийн нэгж, хэлтсийн нэрийг хадгалах зориулалттай
 class Division {
-    string divisionName;
+    string divisionName;  // Хэлтсийн нэр
 
 public:
+    // Constructor: хэлтсийн нэрийг параметрээр авах ба өөр утга өгөгдөөгүй тохиолдолд хоосон утга авна
     Division(string name = "");
+
+    // Getter: хэлтсийн нэрийг буцаах
     string getDivisionName() const;
+
+    // Setter: хэлтсийн нэрийг шинэчлэх
     void setDivisionName(string name);
 };
 
