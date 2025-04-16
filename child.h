348e8774223ff220d5f155dd69a45d85ce0bb9c3
ӨМИРСЕРИК БАЗАРХАН <bazarkhanumirsyerik@gmail.com>
2025-04-16 21:10:23 +0800

    Update child.h

--- child.h
+++ child.h
@@ -1,16 +1,26 @@
 #ifndef CHILD_H
 #define CHILD_H
 
+#include "person.h"    // Person классыг удамшуулахын тулд оруулж ирнэ
 #include <string>
 using namespace std;
 
-class Child {
-    string favoriteToy;
+// Child класс нь Person-оос удамшиж, хүүхдийн дуртай тоглоомын мэдээллийг хадгална.
+class Child : public Person {
+    string favoriteToy;  // Хүүхдийн дуртай тоглоом
 
 public:
-    Child(string toy = "");
+    // Constructor: Нэр, SSN, нас, дуртай тоглоомыг параметрээр хүлээн авна
+    Child(string name = "", string ssn = "", int age = 0, string toy = "");
+
+    // Getter: дуртай тоглоомын нэрийг буцаана
     string getFavoriteToy() const;
+
+    // Setter: дуртай тоглоомыг шинэчлэх
     void setFavoriteToy(string toy);
+
+    // Person классын цэвэр виртуал функцийн хэрэгжилт
+    void printInfo() const override;
 };
 
 #endif
