8b1f7f7537305701fb203bf77b12839710c8c6f3
ӨМИРСЕРИК БАЗАРХАН <bazarkhanumirsyerik@gmail.com>
2025-04-16 21:09:37 +0800

    Update child.cpp

--- child.cpp
+++ child.cpp
@@ -1,13 +1,24 @@
-// Child.cpp
 #include "child.h"
+#include <iostream>
 
+// Constructor: Person классын гишүүд болон favoriteToy-г инициализ хийж өгнө
 Child::Child(string name, string ssn, int age, string toy)
     : Person(name, ssn, age), favoriteToy(toy) {}
 
+// Getter функц: дуртай тоглоомын нэрийг буцаана
 string Child::getFavoriteToy() const {
     return favoriteToy;
 }
 
+// Setter функц: дуртай тоглоомын нэрийг шинэчилнэ
 void Child::setFavoriteToy(const string& toy) {
     favoriteToy = toy;
+}
+
+// Person классын printInfo() виртуал функцийг хэрэгжүүлнэ
+void Child::printInfo() const {
+    cout << "Name: " << getName() << endl;
+    cout << "SSN: " << getSSN() << endl;
+    cout << "Age: " << getAge() << endl;
+    cout << "Favorite Toy: " << favoriteToy << endl;
 }
