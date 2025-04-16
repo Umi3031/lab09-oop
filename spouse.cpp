dbda7e777d7e4a9bbb319a1ea1440da6c20c9662
ӨМИРСЕРИК БАЗАРХАН <bazarkhanumirsyerik@gmail.com>
2025-04-16 21:02:08 +0800

    Update spouse.cpp

--- spouse.cpp
+++ spouse.cpp
@@ -1,13 +1,24 @@
-// Spouse.cpp
-#include "spouse.h"
+#ifndef SPOUSE_H
+#define SPOUSE_H
 
-Spouse::Spouse(string name, string ssn, int age, string anniversaryDate)
-    : Person(name, ssn, age), anniversaryDate(anniversaryDate) {}
+#include "person.h"  // Person классыг ашиглахын тулд оруулж ирж байна
+#include <string>
+using namespace std;
 
-string Spouse::getAnniversaryDate() const {
-    return anniversaryDate;
-}
+// Spouse класс нь Person классыг удамшуулж, гэрлэсэн хүний нэмэлт мэдээлэл хадгална.
+class Spouse : public Person {
+    string anniversaryDate;  // Гэрлэлтийн ой тэмдэглэсэн огноо
 
-void Spouse::setAnniversaryDate(const string& date) {
-    anniversaryDate = date;
-}
+public:
+    // Constructor: нэр, ssn, нас болон гэрлэлт ойг өгч болно
+    Spouse(string name = "", string ssn = "", int age = 0, string anniversaryDate = "");
+
+    // Getter ба Setter функцүүд
+    string getAnniversaryDate() const;             // Гэрлэлт ойг буцаах
+    void setAnniversaryDate(string anniversaryDate); // Гэрлэлт ойг шинэчлэх
+
+    // Person классын pure virtual функцийг хэрэгжүүлнэ
+    void printInfo() const override;
+};
+
+#endif
