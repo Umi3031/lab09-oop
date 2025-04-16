1f8a9e921b030db9002af4e5413f38563fab4571
ӨМИРСЕРИК БАЗАРХАН <bazarkhanumirsyerik@gmail.com>
2025-04-16 21:11:34 +0800

    Update division.cpp

--- division.cpp
+++ division.cpp
@@ -1,12 +1,14 @@
-// Division.cpp
 #include "division.h"
 
+// Constructor: divisionName хувьсагчийг өгөгдсөн нэрээр инициализ хийж байна
 Division::Division(string name) : divisionName(name) {}
 
+// Getter: хэлтсийн нэрийг буцаах
 string Division::getDivisionName() const {
     return divisionName;
 }
 
+// Setter: хэлтсийн нэрийг шинэчлэх
 void Division::setDivisionName(const string& name) {
     divisionName = name;
 }
