2022039f682f383c871d6687ffd2aa88ccef0242
ӨМИРСЕРИК БАЗАРХАН <bazarkhanumirsyerik@gmail.com>
2025-04-16 21:06:27 +0800

    Update jobDescription.cpp

--- jobDescription.cpp
+++ jobDescription.cpp
@@ -1,12 +1,14 @@
-// JobDescription.cpp
 #include "jobdescription.h"
 
+// Constructor: өгөгдсөн тодорхойлолтыг class-ийн description хувьсагчид онооно
 JobDescription::JobDescription(string desc) : description(desc) {}
 
+// description хувьсагчийн одоогийн утгыг буцаана
 string JobDescription::getDescription() const {
     return description;
 }
 
+// description хувьсагчийн утгыг шинэчилнэ
 void JobDescription::setDescription(const string& desc) {
     description = desc;
 }
