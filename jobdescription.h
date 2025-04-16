9413c27bf2149d5742ee7055e5047f432dcf78cc
ӨМИРСЕРИК БАЗАРХАН <bazarkhanumirsyerik@gmail.com>
2025-04-16 21:07:18 +0800

    Update jobdescription.h

--- jobdescription.h
+++ jobdescription.h
@@ -4,12 +4,18 @@
 #include <string>
 using namespace std;
 
+// JobDescription класс нь ажил үүргийн тодорхойлолтыг хадгалах зориулалттай тусдаа класс юм.
 class JobDescription {
-    string description;
+    string description;  // Ажил үүргийн тодорхойлолт
 
 public:
+    // Constructor: анхдагч утга өгөгдөөгүй тохиолдолд хоосон string ашиглана
     JobDescription(string d = "");
+
+    // Getter функц: ажил үүргийн тодорхойлолтыг буцаана
     string getDescription() const;
+
+    // Setter функц: ажил үүргийн тодорхойлолтыг шинэчилнэ
     void setDescription(string d);
 };
 
