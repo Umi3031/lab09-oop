e0673870acbffde6b19e360ee83097b90ba6590f
ӨМИРСЕРИК БАЗАРХАН <bazarkhanumirsyerik@gmail.com>
2025-04-16 20:43:46 +0800

    Create person.cpp

---
+++ person.cpp
@@ -0,0 +1,29 @@
+// Person.cpp
+#include "Person.h"
+
+Person::Person(string name, string ssn, int age)
+    : name(name), ssn(ssn), age(age) {}
+
+string Person::getName() const {
+    return name;
+}
+
+string Person::getSSN() const {
+    return ssn;
+}
+
+int Person::getAge() const {
+    return age;
+}
+
+void Person::setName(const string& newName) {
+    name = newName;
+}
+
+void Person::setSSN(const string& newSSN) {
+    ssn = newSSN;
+}
+
+void Person::setAge(int newAge) {
+    age = newAge;
+}
