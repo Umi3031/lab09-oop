ba3b1f4c6d4d0969e8627c21b7d91a67ac7dc47e
ӨМИРСЕРИК БАЗАРХАН <bazarkhanumirsyerik@gmail.com>
2025-04-16 21:00:38 +0800

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,29 +1,38 @@
-// Person.cpp
 #include "person.h"
 
+// Constructor-ийн хэрэгжүүлэлт
+// name, ssn, age гэсэн утгуудыг class-ийн гишүүн хувьсагчдад оноож өгнө.
 Person::Person(string name, string ssn, int age)
     : name(name), ssn(ssn), age(age) {}
 
+// Гишүүн функцүүдийн хэрэгжүүлэлт
+
+// Нэрийг буцаах
 string Person::getName() const {
     return name;
 }
 
+// SSN-г буцаах
 string Person::getSSN() const {
     return ssn;
 }
 
+// Нас буцаах
 int Person::getAge() const {
     return age;
 }
 
+// Нэрийг шинэчилж оноох
 void Person::setName(const string& newName) {
     name = newName;
 }
 
+// SSN-г шинэчилж оноох
 void Person::setSSN(const string& newSSN) {
     ssn = newSSN;
 }
 
+// Насыг шинэчилж оноох
 void Person::setAge(int newAge) {
     age = newAge;
 }
