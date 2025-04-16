8cc45a66d2a7b59105c9c8eba1dd2333e0beb974
ӨМИРСЕРИК БАЗАРХАН <bazarkhanumirsyerik@gmail.com>
2025-04-16 20:36:34 +0800

    Create employee.h.cpp

---
+++ employee.h.cpp
@@ -0,0 +1,38 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include "Person.h"
+#include "Spouse.h"
+#include "Child.h"
+#include "Division.h"
+#include "JobDescription.h"
+#include <vector>
+
+class Employee : public Person {
+    string companyID;
+    string title;
+    string startDate;
+    Division* division;
+    vector<JobDescription> jobs;
+    Spouse* spouse;
+    vector<Child> children;
+
+    // Static count of all employees
+    static int employeeCount;
+
+public:
+    Employee(string n, string s, int a, string id, string t, string d);
+    ~Employee();
+
+    void setDivision(Division* d);
+    void addJob(JobDescription j);
+    void setSpouse(Spouse* s);
+    void addChild(Child c);
+
+    void printInfo() const override;
+
+    // Static method
+    static int getEmployeeCount();
+};
+
+#endif
